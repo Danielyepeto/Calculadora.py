1b05472fb181062af030f13df400583a2a827c97
Daniel Falcon <99775135+Danielyepeto@users.noreply.github.com>
2022-02-27 20:38:25 -0400

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,34 @@
+/******************************************************************************
+
+                              Online C++ Compiler.
+               Code, Compile, Run and Debug C++ program online.
+Write your code in this editor and press "Run" button to compile and execute it.
+
+*******************************************************************************/
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int numero1;
+    int numero2;
+    
+    cout << "ingrese el primer numero:  ";
+    cin >> numero1;
+    
+    cout<< "Ingrese el segundo numero:  ";
+    cin >> numero2;
+    
+    cout << "Resultado de la suma : " << (numero1 + numero2) << endl;
+    
+    cout << "Resultado de la resta : " << (numero1 - numero2) << endl;
+    
+    cout << "Resultado de la multiplicacion : " << (numero1 * numero2) << endl;
+    
+    cout << "Resultado de la division : " << (numero1 / numero2) << endl;
+    
+
+    return 0;
+}
